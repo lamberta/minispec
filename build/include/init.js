2bc3b9930e7d9169b9149f0ce0aa306ed2907cbd
Billy Lamberta <b@lamberta.org>
2013-08-26 02:35:55 -0700

    Add initial js/css to hide advanced section.

--- build/include/init.js
+++ build/include/init.js
@@ -40,13 +40,29 @@ $(document).ready(function () {
 		}
 	});
 	***/
+
+	/* Minimize advanced section
+	 */
+	var adv_header = $('h2#advanced'),
+			expand_switch = $('<span class="advanced-switch">&nbsp;⊕</span><span class="advanced-switch" style="display:none">&nbsp;⊖</span>'),
+			adv_section = adv_header.nextUntil('h2').wrapAll('<div class="advanced-hidden"/>');
+	adv_header.append(expand_switch);
+	adv_section.hide();
+	adv_header.click(function () {
+		adv_section.toggle();
+		expand_switch.toggle();
+	});
+	//hide sidebar heading
+	adv_section.filter('h3').each(function (i, elem) {
+		$('aside nav a[href="#'+ elem.id +'"]').hide();
+	});
 	
 	/* typeahead: https://github.com/twitter/typeahead.js
 	 */ 
 	var search_form = navbar.find('form');
 	search_form.find('input').typeahead({
 		name: 'index',
-    prefetch: './search-index.json'
+		prefetch: './search-index.json'
 	});
 	search_form.find('input').on('typeahead:selected typeahead:autocompleted', function (evt, data) {
 		console.log('event type', evt.type);
